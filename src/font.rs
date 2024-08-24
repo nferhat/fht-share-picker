953f251f7c2fea02e0544e91fa421543e112647e
nferhat <nferhat20@gmail.com>
2024-08-24 13:42:08 +0100

    Update fonts
    Migrate to my own iosevka variant

--- src/font.rs
+++ src/font.rs
@@ -1,18 +1,18 @@
 use std::borrow::Cow;
 
 // Font declarations
-pub static MONO: iced::Font = iced::Font::with_name("Iosevka Term");
+pub static MONO: iced::Font = iced::Font::with_name("Fht Mono");
 pub static MONO_BOLD: iced::Font = iced::Font {
     weight: iced::font::Weight::Bold,
-    ..iced::Font::with_name("Iosevka Term")
+    ..iced::Font::with_name("Fht Mono")
 };
 
 pub fn load() -> Vec<Cow<'static, [u8]>> {
     vec![
-        include_bytes!("../assets/fonts/IosevkaTerm-Regular.ttf")
+        include_bytes!("../assets/fonts/FhtMono-Regular.ttf")
             .as_slice()
             .into(),
-        include_bytes!("../assets/fonts/IosevkaTerm-Bold.ttf")
+        include_bytes!("../assets/fonts/FhtMono-Bold.ttf")
             .as_slice()
             .into(),
     ]
